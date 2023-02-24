95b68ce279c66cf56124074f9d0dfc50a15e0993
Aaron <aaron@framelunch.jp>
2023-02-25 00:38:05 +0900

    hr justification

--- src/print_ext/hr.py
+++ src/print_ext/hr.py
@@ -1,30 +1,47 @@
 from .borders import Borders, BorderDfn
 from .context import Context
-from .line import Line
+from .flex import Flex
+from .line import Line, Just
 
-class HR(Borders):
+class HR(Flex):
 
-    default_bdr = BorderDfn(m='01', l='│││┤┤[[[[[', r='│││├├]]]]]', t='─-')
+    default_bdr = BorderDfn(l='\n┤│\n\n\n[[\n\n', r='\n├│\n\n\n]]\n\n', t='─-')
 
     ctx_defaults = Context.defaults(border=default_bdr)
     
     @property
     def width(self):
-        return self['width_max'] or (super().width + 4)
+        return self['width_max'] or (super().width + 6)
 
 
     def flatten(self, w=0, h=0, **kwargs):
-        mw = w or self['width_max'] or (super().width + 4)
-        mh = h or self.height
-        style = self['border_style']
-        side = self['border'].t[7 if self['ascii'] else 2]
-        for i,l in enumerate(super().flatten(h=h,**kwargs)):
-            pad = side if i == mh-1 else ' '
-            rhs = (mw-l.width)//2
-            lhs = pad*(mw-l.width-rhs)
-            rhs = pad*rhs
-            print(f"I:{i} _{l}_ {mw-l.width} {lhs!r} {rhs!r}")
-            yield l if not (lhs or rhs) else Line(parent=self)(lhs,style=style)(l)(rhs,style=style)
-
-
+        nom = super().width
+        mw = w or self['width_max'] or (nom + 6)
+        innerw = min(mw-6, nom)
+        if innerw <= 0:
+            yield from super().flatten(w=mw, h=h, **kwargs)
+            return
+        flat = list(super().flatten(w=innerw, h=h, **kwargs))
+        if not flat: return []
+        ascii, bdr, style = self['ascii'], self['border'], self['border_style']
+        just = Just(self['just'],'|~')
+        hrv = just.pad_v(len(flat)-1)
+        bar = bdr.t[7 if ascii else 2]
+        bar_l = bdr.t[5 if ascii else 0]
+        bar_r = bdr.t[8 if ascii else 3]
+        join_l = bdr.l[6 if ascii else 1]
+        box_l = bdr.l[7 if ascii else 2]
+        join_r = bdr.r[6 if ascii else 1]
+        box_r = bdr.r[7 if ascii else 2]
+        left = just.pad_h(mw-innerw) or 3
+        if left == mw-innerw: left = mw-innerw-3
+        #print(f"JUST  {just}  bdr: {style} {bdr}  {left}/{innerw}/{mw}  h:{len(flat)}")
+        for i,l in enumerate(flat):
+            if i == hrv:
+                lhs = bar_l + bar*(left-3) + join_l + ' '
+                rhs = ' ' + join_r + bar*(mw-innerw-left-3) + bar_r
+            else:
+                lhs = ' '*(left-2) + box_l + ' '
+                rhs = ' ' + box_r + ' '*(mw-innerw-left-2)
+            yield Line(parent=self)(lhs,style=style)(l)(rhs,style=style)
 
--- tests/hr_test.py
+++ tests/hr_test.py
@@ -5,25 +5,56 @@ from print_ext.flex import Flex
 from print_ext.text import Text
 from .printer_test import _printer
 
-def test_hr_hello():
-    h = HR('hello', ascii=True, style='y')
-    assert([l.styled() for l in h.flatten(w=10)] == [('--[hello]-',[SM('y',0,10), SM('dem',0,3), SM('dem',8,10)])])
 
-    h = HR('hello\vto the\vworld', ascii=True, style='y')
-    s = [l.styled() for l in h.flatten(w=11)]
-    print('\n'.join(x[0] for x in s))
-    assert(s[0] == ('  [hello ] ',[SM('y',0,11), SM('dem',0,3), SM('dem',9,11)]))
-    assert(s[1] == ('  [to the] ',[SM('y',0,11), SM('dem',0,3), SM('dem',9,11)]))
-    assert(s[2] == ('--[world ]-',[SM('y',0,11), SM('dem',0,3), SM('dem',9,11)]))
-
-
-def test_hello_pretty():
+def test_hr_hello_pretty():
     o, p = _printer(width=10, ascii=True)
     p.hr('hi')
-    assert(o.getvalue() == '---[hi]---\n')
+    assert(o.getvalue() == '--[ hi ]--\n')
+
+
+
+def test_hr_just_h():
+    o,p = _printer(width=10, ascii=True)
+    p.hr('hi', just='<')
+    print(o.getvalue())
+    assert(o.getvalue() == '-[ hi ]---\n')
+    h = HR('hi', just='|', ascii=True, width_max=10, style='y')
+    assert([f.styled() for f in h.flatten()][0] == ('--[ hi ]--', [SM('y',0,10)]))
+    assert([f.styled() for f in h.flatten(w=9)][0] == ('-[ hi ]--', [SM('y',0,9)]))
+    h['just'] = ':'
+    assert([f.styled() for f in h.flatten(w=9)][0] == ('--[ hi ]-', [SM('y',0,9)]))
+    h['just'] = '>'
+    assert([f.styled() for f in h.flatten()][0] == ('---[ hi ]-', [SM('y',0,10)]))
+
+
+
+def test_hr_just_v():
+    h = HR('a\vbbb\vc\v', ascii=True, width_max=11)
+    assert([f.styled()[0] for f in h.flatten()] == ['  [ a   ]  ', '  [ bbb ]  ','--[ c   ]--','  [     ]  '])
+    h['just'] = '_'
+    assert([f.styled()[0] for f in h.flatten()] == ['  [ a   ]  ', '  [ bbb ]  ','  [ c   ]  ','--[     ]--'])
+    h['just'] = '^'
+    assert([f.styled()[0] for f in h.flatten()] == ['--[ a   ]--', '  [ bbb ]  ','  [ c   ]  ','  [     ]  '])
+    h['just'] = '-'
+    h['ascii'] = False
+    assert([f.styled()[0] for f in h.flatten()] == ['  │ a   │  ', '──┤ bbb ├──','  │ c   │  ','  │     │  '])
+    h['just'] = '<'
+    assert([f.styled()[0] for f in h.flatten()] == [' │ a   │   ', ' │ bbb │   ','─┤ c   ├───',' │     │   '])
+
+
+
+def test_hr_small():
+    h = HR('hello world', ascii=True)
+    assert([f.styled()[0] for f in h.flatten()] == ['-[ hello world ]-'])
+    assert([f.styled()[0] for f in h.flatten(w=11)] == ['-[ he~ld ]-'])
+    assert([f.styled()[0] for f in h.flatten(w=7)] == ['-[ ~ ]-'])
+    assert([f.styled()[0] for f in h.flatten(w=6)] == ['hel~ld'])
+    h['wrap']=True
+    assert([f.styled()[0] for f in h.flatten(w=11)] == [' [ hello ] ', '-[ \\  wo ]-', ' [ \\ rld ] '])
+
 
 
 def test_hr_clone():
-    h = HR('bob')
-    assert([str(x) for x in h.flatten()] == ['──┤bob├──'])
-    assert([str(x) for x in Text(h,h).flatten()] == ['──┤bob├────┤bob├──'])
+    h = HR('bob', ascii=False)
+    assert([str(x) for x in h.flatten()] == ['─┤ bob ├─'])
+    assert([str(x) for x in Text(h,h).flatten()] == ['─┤ bob ├──┤ bob ├─'])
