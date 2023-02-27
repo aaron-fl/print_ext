4202998092c082a472f168a6dc46d87f925eb491
Aaron <aaron@framelunch.jp>
2023-02-27 12:07:09 +0900

    pretty functions

--- src/print_ext/pretty.py
+++ src/print_ext/pretty.py
@@ -1,46 +1,24 @@
-from .widget import Widget
 from .table import Table
 
 
-
-def pretty(val):
-    if isinstance(val, tuple): return PrettyList(val, '()')
-    if isinstance(val, list):  return PrettyList(val, '[]')
-    if hasattr(val, 'items'): return PrettyDict(val)
+def pretty(val, depth=0):
+    if isinstance(val, tuple): return _pretty_list(val, '()', depth) if val else 'tuple()'
+    if isinstance(val, list):  return _pretty_list(val, '[]', depth) if val else '[]'
+    if hasattr(val, 'items'): return _pretty_dict(val, depth) if val else '{}'
     return val
 
 
-class PrettyList(Widget):
-    def __init__(self, val, paren='[]', **kwargs):
-        self.val = val
-        self.paren = paren
-        super().__init__(**kwargs)
-
-    
-    def flatten(self, w=0, h=0, **kwargs):
-        t = Table(0.0,0,tmpl='kv')
-        for i,v in enumerate(self.val):
-            t(f'\bdem {self.paren[0]}', i,f'\bdem {self.paren[1]} ', '\t', pretty(v), '\t')
-        yield from t.flatten(w=w,h=h,**kwargs)
+def _pretty_list(val, paren, depth):
+    t = Table(0.0, 0, tmpl='')
+    t.cell('C0', style=str(depth%3 + 1), just='>')
+    for i,v in enumerate(val):
+        t(f'\bdem {paren[0]}', i,f'\bdem {paren[1]} ', '\t', pretty(v, depth+1), '\t')
+    return t
 
 
-    def clone(self, **kwargs):
-        return self.__class__(self.val, self.paren, **kwargs)
-
-
-
-class PrettyDict(Widget):
-    def __init__(self, val, **kwargs):
-        self.val = val
-        super().__init__(**kwargs)
-
-    
-    def flatten(self, w=0, h=0, **kwargs):
-        t = Table(0.0, 0, tmpl='kv')
-        for k,v in self.val.items():
-            t(k,' ', '\t',pretty(v),'\t')
-        yield from t.flatten(w=w,h=h,**kwargs)
-
-
-    def clone(self, **kwargs):
-        return self.__class__(self.val, **kwargs)
+def _pretty_dict(val, depth):
+    t = Table(0.0, 0, tmpl='')
+    t.cell('C0', style=str(depth%3 + 1), just='>')
+    for k,v in val.items():
+        t(k,' ', '\t', pretty(v, depth+1),'\t')
+    return t
